9dd01aded32513f7a6588b691e0ccbb9ab40d93d
Mital Andipara <mital@logisticinfotech.co.in>
2025-03-26 19:04:51 +0530

    add layout file

---
+++ src/app/(user)/layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Userlayout = ({ children }) => {
+    return (
+        <div className="max-w-[393pt] h-full max-h-[852pt] m-auto w-full bg-white ">
+            {children}
+        </div>
+    );
+};
+
+export default Userlayout;
