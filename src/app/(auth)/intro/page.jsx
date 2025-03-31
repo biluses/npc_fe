6ba62a6cb6ef264b20d100bfc432bd4384065b96
Mital Andipara <mital@logisticinfotech.co.in>
2025-03-31 18:58:52 +0530

    responsive design of intro page

--- src/app/(auth)/intro/page.jsx
+++ src/app/(auth)/intro/page.jsx
@@ -1,116 +1,95 @@
 "use client";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-const images = [
-    "/images/introImage1.png",
-    "/images/introImage2.png",
-    "/images/introImage3.png",
-];
-
-const texts = [
-    { title: "Construye tu armario digital", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-    { title: "Envía y recibe tokens de positividad", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-    { title: "Forma parte de la comunidad", description: "Organize your wardrobe in a digital format." },
-];
-const colorSequence = ["text-blue-600", "text-red-600", "text-green-600"];
-
-const Carousel = () => {
+const Slider = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [colorIndex, setColorIndex] = useState(0);
     const router = useRouter();
+    const slides = [
+        { image: "/images/introImage1.png", title: "Construye tu armario digital", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", color: "text-[#FF00F2]" },
+        { image: "/images/introImage2.png", title: "Envía y recibe tokens de positividad", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", color: "text-[#00FBFB]" },
+        { image: "/images/introImage3.png", title: "Forma parte de la comunidad", description: "Organize your wardrobe in a digital format.", color: "text-[#EEFF00]" }
+    ];
 
-    // Function to go to the next slide
     const nextSlide = () => {
-        if (currentIndex === images.length - 1) {
-            router.push("/login"); // Redirect to the next route
-        } else {
-            setCurrentIndex((prevIndex) => prevIndex + 1);
-            setColorIndex((prevIndex) => (prevIndex + 1) % colorSequence.length);
-        }
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
     };
 
     const setSlide = (index) => {
         setCurrentIndex(index);
-        setColorIndex(index);
     };
 
+    const colorSequence = ["text-blue-600", "text-red-600", "text-green-600"];
     return (
-        <div className="intro-main-div">
-            {/* Image Container */}
-            <div className="intro-silder-main-div">
-                {images.map((src, index) => (
-                    <img
-                        key={index}
-                        src={src}
-                        alt={`Slide ${index + 1}`}
-                        className={`silder-images ${index === currentIndex ? "opacity-100" : "opacity-0"}`}
-                    />
-                ))}
-                <button className="close-button" onClick={() => router.push("/login")}>
-                    <img src="/images/close_icon.svg" alt="close" />
-                </button>
-            </div>
-            {/* Text for current slide */}
-            <div className="intro-details">
-                <h4 className="intro-title">{texts[currentIndex].title}</h4>
-                <p className="intro-description">{texts[currentIndex].description}</p>
-            </div>
-            <div className="silder-container">
-                <div className="silder-dot">
-                    {images.map((_, index) => (
-                        <button
-                            key={index}
-                            className={`silder-dot-button ${index === currentIndex ? "bg-white" : "bg-neutral-700"}`}
-                            onClick={() => setSlide(index)}
-                        ></button>
+        <div className="flex flex-col w-full min-h-screen bg-black">
+            <div className="relative overflow-hidden">
+                <div className="flex transition-transform duration-500" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+                    {slides.map((slide, index) => (
+                        <div key={index} className="min-w-full flex flex-col">
+                            <div className="h-[300pt]">
+                                <img src={slide.image} alt="" className="w-full h-full object-cover" />
+                                <button className="close-button" onClick={() => router.push("/login")}>
+                                    <img src="/images/close_icon.svg" alt="close" />
+                                </button>
+                            </div>
+                            <div className="bg-black text-white p-6 w-full text-center">
+                                <h4 className="intro-title">{slide.title}</h4>
+                                <p className="intro-description">{slide.description}</p>
+                            </div>
+                        </div>
                     ))}
                 </div>
+            </div>
+            <div className="flex justify-center mt-7 space-x-2">
+                {slides.map((_, index) => (
+                    <button
+                        key={index}
+                        className={`w-3 h-3 rounded-full ${index === currentIndex ? "bg-white" : "bg-neutral-700"}`}
+                        onClick={() => setSlide(index)}
+                    ></button>
+                ))}
+            </div>
 
-                <div className="next-progess-button" onClick={nextSlide}>
-                    <svg className="size-full -rotate-90" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
-                        {/* Outer Circle */}
-                        <circle cx="18" cy="18" r="10" fill="none" className="stroke-current text-neutral-700" strokeWidth="2"></circle>
+            <div className="next-progess-button" onClick={nextSlide}>
+                <svg className="-rotate-90" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="18" cy="18" r="10" fill="none" className="stroke-current text-neutral-700" strokeWidth="2"></circle>
+                    <circle
+                        cx="18"
+                        cy="18"
+                        r="10"
+                        fill="none"
+                        className={`stroke-current ${slides[currentIndex].color}`}
+                        strokeWidth="2"
+                        strokeDasharray="100"
+                        strokeDashoffset="59"
+                        strokeLinecap="round"
+                    />
+                </svg>
 
-                        {/* Inner Circle with Dynamic Color */}
-                        <circle
-                            cx="18"
-                            cy="18"
-                            r="10"
-                            fill="none"
-                            className={`stroke-current ${colorSequence[colorIndex]}`} // Change color based on state
-                            strokeWidth="2"
-                            strokeDasharray="100"
-                            strokeDashoffset="59"
-                            strokeLinecap="round"
-                        />
-                    </svg>
-
-                    <div className="progress-arrow-div">
-                        <button type="button" className="progess-arrow-btn">
-                            <svg
-                                className="w-5 h-5"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 14 10"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                                />
-                            </svg>
-                            <span className="sr-only">Next</span>
-                        </button>
-                    </div>
+                <div className="progress-arrow-div">
+                    <button type="button" className={`progess-arrow-btn ${slides[currentIndex].color}`}>
+                        <svg
+                            className="w-5 h-5"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 14 10"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                fill="currentColor"
+                                d="M1 5h12m0 0L9 1m4 4L9 9"
+                            />
+                        </svg>
+                        <span className="sr-only">Next</span>
+                    </button>
                 </div>
             </div>
         </div>
-
     );
 };
 
-export default Carousel;
+export default Slider;
