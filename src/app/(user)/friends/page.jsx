0e46ccd772dae047637062d2986be4911b45b8de
Mital Andipara <mital@logisticinfotech.co.in>
2025-03-31 18:57:14 +0530

    design changes in friend page

--- src/app/(user)/friends/page.jsx
+++ src/app/(user)/friends/page.jsx
@@ -25,13 +25,19 @@ const FriendPage = () => {
                 </button>
                 <h1 className="mx-auto text-3xl font-bold">Amigos</h1>
             </div>
-            <div>
+            {/* <div>
+                <div className="password-main-div">
+                    <input type="text" name="search" placeholder="Buscar" className="relative common-input" />
+                    <img src="/images/search_icon.svg" className="password-icon-img pr-2" alt="search_icon" />
+                </div>
+            </div> */}
+            <div className="sticky top-[68px] bg-white z-10">
                 <div className="password-main-div">
                     <input type="text" name="search" placeholder="Buscar" className="relative common-input" />
                     <img src="/images/search_icon.svg" className="password-icon-img pr-2" alt="search_icon" />
                 </div>
             </div>
-            <div className="flex flex-wrap xs:justify-around my-5 h-auto gap-3">
+            {/* <div className="pb-28 flex flex-wrap w-full bg-pink-400 my-5 h-auto gap-3 mx-auto">
                 {friends.map((friend) => (
                     <div key={friend.id} className="xs:w-[80pt] xs:h-[80pt] w-[50pt] h-[50pt] flex relative">
                         <img
@@ -44,7 +50,22 @@ const FriendPage = () => {
                         )}
                     </div>
                 ))}
+            </div> */}
+            <div className="pb-28 w-full my-5 mx-auto grid grid-cols-3 xs:grid-cols-4 sm:grid-cols-4 gap-3">
+                {friends.map((friend) => (
+                    <div key={friend.id} className="aspect-square relative">
+                        <img
+                            src={friend.img}
+                            alt="Friend"
+                            className="w-full h-full rounded-full object-cover border-2 border-gray-400"
+                        />
+                        {friend.online && (
+                            <span className="absolute top-2 right-2 w-4 h-4 bg-cyan border-2 rounded-full border-white"></span>
+                        )}
+                    </div>
+                ))}
             </div>
+
             <BottomNavbar />
         </div>
     );
